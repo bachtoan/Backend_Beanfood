4a9f4c92ef7e951a93194ee759d1b84719c451f3
datha <dath33603@gmail.com>
2023-12-17 23:02:54 +0700

    sua voucher

--- controllers/notify.controller.js
+++ controllers/notify.controller.js
@@ -12,9 +12,13 @@ exports.getNotifyById = async (req, res, next) => {
     return res.status(500).json({ msg: error.message });
   }
 };
-exports.postNotify = async (data) => {
+exports.postNotify = async (req, res) => {
+  const dt = {
+    idUser: req.body.userId,
+    money: req.body.toltalprice,
+  };
   try {
-    await notifyModel.notifyModel.create(data);
+    notifyModel.notifyModel.create(dt).then((dta) => {});
   } catch (error) {
     return res.status(500).json({ msg: error.message });
   }
